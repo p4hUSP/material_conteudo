688904a268360bb5c06a377016b72bb0e7282b5a
Rafael Coelho <rafael.coelho.x@gmail.com>
2018-04-14 23:57:15 -0400

    melhora da função

--- build.R
+++ build.R
@@ -6,26 +6,29 @@
 rm(list = ls())
 
 create_content <- function(dir = "./tutoriais"){
-  system("rm -Rf ./content") #Exclui a pasta de content
-  file <- list.files("./tutoriais/")
-  dir_file <- stringr::str_remove(file, "\\.md$|\\.Rmd$")
-  for(i in seq_along(file)){
-    suppressWarnings(dir.create(sprintf("./content/%s", dir_file[[i]]),
-                                recursive = TRUE))
-    if(stringr::str_detect(file[[i]], "\\.md$")){
-      create_md(dir_file[[i]])
-    } else if(stringr::str_detect(file[[i]], "\\.Rmd$")){
-      create_Rmd(file[[i]], dir_file[[i]])
+  tu_dirs <- list.files(".", "^tu\\.") #lista os diretórios dos tutorias
+  tu_files <- purrr::map_chr(tu_dirs, list.files) #lista os arquivos dentro dos diretórios
+  content_dir <- stringr::str_remove(tu_dirs, "^tu\\.") #cria uma lista com o futuro nome dos diretórios dos tutorias
+  for(i in seq_along(content_dir)){
+    #Cria o diretório para o tutoria ldentro de ./content
+    suppressWarnings(dir.create(sprintf("./content/%s", content_dir[[i]]),recursive = TRUE))
+    #Testa se o arquivo é um .md ou um .Rmd
+    if(stringr::str_detect(tu_files[[i]], "\\.md$")){
+      create_md(tu_dirs[[i]], tu_files[[i]], content_dir[[i]])
+    } else if(stringr::str_detect(tu_files[[i]], "\\.Rmd$")){
+      create_Rmd(tu_dirs[[i]], tu_files[[i]], content_dir[[i]])
     }
   }
 }
 
-create_md <- function(dir_file){
-  system(sprintf("cp ./tutoriais/%s.md ./content/%s/index.md", dir_file, dir_file))
+#Transforma o .md em um index.md
+create_md <- function(dir, file, content_dir){
+  system(sprintf("cp ./%s/%s ./content/%s/index.md", dir, file, content_dir))
 }
 
-create_Rmd <- function(file, dir_file){
-  ezknitr::ezknit(stringr::str_c("./tutoriais/", file),
+#cria um .md a partir de um .Rmd
+create_Rmd <- function(dir, file, dir_file){
+  ezknitr::ezknit(stringr::str_c("./", dir, "/", file),
                   out_dir = stringr::str_c("./content/", dir_file),
                   fig_dir = "figures",
                   keep_html = FALSE)
